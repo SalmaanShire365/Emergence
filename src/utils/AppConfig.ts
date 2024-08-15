ff7271d73256f10d6006aeed5429bd84b8a09952
SalmaanShire365 <salmaanshire@gmail.com>
2024-08-15 18:18:50 -0500

    message

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,6 @@
+export const AppConfig = {
+  site_name: "Emergence",
+  title: "Dungeons & Dragons Tool Maker",
+  description: "Making Campaigns Easier to Make & Even Easier To Play In",
+  locale: "en",
+};
